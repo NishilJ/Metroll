d253cccda7da50462bc8ae66dc7843bf7a4bdfcc
Syed Kabir <syedkabir909@gmail.com>
2024-11-06 17:26:24 -0600

    Create Routes.tsx
    Initial construction of the Routes Page for Metroll Project.
    
    Blank state currently, next step is to allow user activity and build UI skeleton using the wireframe.

---
+++ src/pages/Routes.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { Flex, TextField, Button, View, Heading } from '@adobe/react-spectrum';
+
+const Routes : React.FC = () => {
+    const [routeNumber, setRouteNumber] = useState('');
+
+
+
+}
+
+export default Routes;
